3133e5d38d8499dac5b61d7bfde086b71e3d5682
MyAenan <55648963+MyAenan@users.noreply.github.com>
2022-11-15 22:20:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ var sumcache = [];
 var eset = 25;
 var load_sums_wait = 0;
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
 
 $(document).ready(function() {
 	
